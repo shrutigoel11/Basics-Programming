27db59a33d28fdd8af504bba7a2e3d4f77ded194
shru200411 <112899970+shru200411@users.noreply.github.com>
2023-05-05 11:43:06 +0530

    5-may-update

--- C++/STL/baics.cpp
+++ C++/STL/baics.cpp
@@ -1,4 +1,5 @@
-/*STL-----> STANDARD TEMPLATE LIBRARY
+/*
+STL-----> STANDARD TEMPLATE LIBRARY
 Generic Classes + Generic Functions= STL
 COMPONENTS :Containers,Algorithms,Iterators
 
@@ -6,4 +7,8 @@ COMPONENTS :Containers,Algorithms,Iterators
   Types of Containers:Sequence:List,Vector,Deque; Associative:Set,Multiset,Map,Multimap ; Derived:Stack,Queues,Priority_queue 
 
 ALGORITHM:procedure used to process data in containers,implemented by template functions,act on containers
-ITERATORS:object(like pointer) that is used to point elements in container ++ / --, acts as a bridge b/w container and algorithm*/
+ITERATORS:object(like pointer) that is used to point elements in container ++ / --, acts as a bridge b/w container and algorithm
+          syntax: conatiner_type < parameters_list > :: iterator iterator_name;
+          
+STL needs using namespace std; (below the declarations of headers)               
+*/
